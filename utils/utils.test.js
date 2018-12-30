7f1f748cfb489daf3d16745b90b03944b6a0d10a
Andres <andrescruz.tech@gmail.com>
2018-12-30 18:53:27 -0300

    Added assertion library expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,21 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
-    if(res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
-it('should return the square', () => {
-    let res = utils.square(8);
-    if(res !== 16) {
-        throw new Error(`Expected 16, but got ${res}`);
-    }
+it('should square a number', () => {
+    let res = utils.square(4);
+    expect(res).toBe(16).toBeA('number');
+});
+
+it('should set firstName and lastName', () => {
+    let user = {location: 'Buenos Aires', age: 34};
+    let res = utils.setName(user, 'Andrés Cruz')
+    expect(res).toInclude({
+        firstName: 'Andrés',
+        lastName: 'Cruz'
+    });
 });
