c47717437115c809e88e1a9643e08a5f5bc4c712
Andres <andrescruz.tech@gmail.com>
2018-12-30 19:51:49 -0300

    Added describe()

--- server/server.test.js
+++ server/server.test.js
@@ -1,29 +1,39 @@
 const request = require('supertest');
 const expect = require('expect');
+const app = require('./server').app;
 
-var app = require('./server').app;
-
-it('should return hello world response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found.'
-        })
-    })
-    .end(done);
-});
-
-it('should return my user object', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Andrés',
-            age: 34
+describe('Server', () => {
+    describe('#GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    })
+                })
+                .end(done);
         });
-    })
-    .end(done);
+    });
+
+    describe('#GET /users', () => {
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Andrés',
+                        age: 34
+                    });
+                })
+                .end(done);
+        });
+    });
 });
+
+
+
+
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,21 +1,24 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    let res = utils.add(33, 11);
-    expect(res).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
+    it('should add two numbers', () => {
+        let res = utils.add(33, 11);
+        expect(res).toBe(44).toBeA('number');
+    });
 
-it('should square a number', () => {
-    let res = utils.square(4);
-    expect(res).toBe(16).toBeA('number');
-});
+    it('should square a number', () => {
+        let res = utils.square(4);
+        expect(res).toBe(16).toBeA('number');
+    });
 
-it('should set firstName and lastName', () => {
-    let user = {location: 'Buenos Aires', age: 34};
-    let res = utils.setName(user, 'Andrés Cruz')
-    expect(res).toInclude({
-        firstName: 'Andrés',
-        lastName: 'Cruz'
+    it('should set firstName and lastName', () => {
+        let user = { location: 'Buenos Aires', age: 34 };
+        let res = utils.setName(user, 'Andrés Cruz')
+        expect(res).toInclude({
+            firstName: 'Andrés',
+            lastName: 'Cruz'
+        });
     });
 });
+
